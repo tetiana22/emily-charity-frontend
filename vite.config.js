baa8c783fc53a4d9d680b0418b59b7125db0f592
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-08-14 14:49:50 +0100

    VITE

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig(({ command }) => {
           entryFileNames: 'main.js',
         },
       },
-      outDir: './dist',
+      outDir: '../dist',
     },
     plugins: [injectHTML(), FullReload(['./src/*.html'])],
   };
